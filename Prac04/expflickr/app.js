f834d0798cd5dd657439fd6667b536ca2e942e5c
don <misurakalu@gmail.com>
2022-08-22 22:12:15 +1000

    still working on prac 05

--- Prac04/expflickr/app.js
+++ Prac04/expflickr/app.js
@@ -7,15 +7,24 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 app.get('/', (req, res) => {
-    const str =  'XXXX';
+    const str = '<!DOCTYPE html>' +
+        '<html><head><title>Flickr Demo</title></head>' +
+        '<body>' +
+        '<h1>' + 'The Flickr API Demo' + '</h1>' +
+        'Usage: http://localhost:3000/search/query/number <br>' +
+        '<ul>' + '<li>query - corresponds to Flickr tags</li>' +
+        '<li>number - max number of results returned</li>' +
+        '<li>Example: <a href="http://localhost:3000/search/goldenretriever/100">http://localhost:3000/search/golden-retriever/100</a></li>' +
+        '</ul>' +
+        '</body></html>';
 
-    res.writeHead(200,{'content-type': 'text/html'});
+    res.writeHead(200, { 'content-type': 'text/html' });
     res.write(str);
     res.end();
 });
 
-app.use('/search?',flickrRouter); 
+app.use('/search?', flickrRouter);
 
-app.listen(port, function () {
+app.listen(port, function() {
     console.log(`Express app listening at http://${hostname}:${port}/`);
 });
--- Prac04/expflickr/routes/flickr.js
+++ Prac04/expflickr/routes/flickr.js
@@ -1,14 +1,18 @@
 const express = require('express');
-
+const https = require('https');
 const router = express.Router();
 
 router.get('/:query/:number', (req, res) => {
-            res.write(req.params.query + ":" + req.params.number);
-            res.end();
+    const options = createFlickrOptions(req.params.query, req.params.number);
+    const flickReq = https.request(options, (flickRes) => {
+        //This is the body of the callback for dealing with the results
+        //from the Flickr API. We will assemble the response as it comes in,
+        //parse it like we did last week and create the page.
+    });
+    flickReq.on('error', (e) => {
+        console.error(e);
+    });
+    flickReq.end();
 });
 
 module.exports = router;
-
-
-
-
