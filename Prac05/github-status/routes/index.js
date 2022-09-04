8ec1dc47adf6e835d6db2b929013cd7fa6bc7b78
don <misurakalu@gmail.com>
2022-09-04 14:49:04 +1000

    finished pug exercises for week 6

--- Prac05/github-status/routes/index.js
+++ Prac05/github-status/routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+    const url = 'https://kctbh9vrtdwd.statuspage.io/api/v2/status.json';
+    axios.get(url)
+        .then((response) => {
+            const rsp = response.data;
+            res.render('index', { rsp });
+        })
+        .catch((error) => {
+            res.render('error', { error });
+        })
 });
-
 module.exports = router;
